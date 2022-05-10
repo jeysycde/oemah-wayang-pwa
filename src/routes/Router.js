66d10b578356e5dc44871eb3c92b187e005b3c1b
xarthaa <manager.arthachan@gmail.com>
2022-05-10 11:07:50 +0800

    textInput

--- src/routes/Router.js
+++ src/routes/Router.js
@@ -1,12 +1,10 @@
 import { Route } from "react-router-dom";
 import Home from "../user/home/Home";
-import DetailProduct from "../user/products/DetailProduct";
 
 const Router = () => {
   return (
     <>
       <Route path={"/"} component={Home} exact />
-      <Route path={"/products/:slug"} component={DetailProduct} exact />
     </>
   );
 };
