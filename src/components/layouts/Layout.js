a3c2967ee72bcdc76b5eab66d3e3a0321cd6d4bb
xarthaa <manager.arthachan@gmail.com>
2022-05-10 10:59:33 +0800

    toaster

--- src/components/layouts/Layout.js
+++ src/components/layouts/Layout.js
@@ -1,10 +1,12 @@
 import Header from "../Header";
 import Footer from "./Footer";
+import { Toaster } from "react-hot-toast";
 
 const Layout = (props) => {
   return (
     <>
       <Header />
+      <Toaster position="top-center" reverseOrder={false} />
       <div className={`${props.className} md:mt-12 mt-4 mx-2`}>
         {props.children}
       </div>
